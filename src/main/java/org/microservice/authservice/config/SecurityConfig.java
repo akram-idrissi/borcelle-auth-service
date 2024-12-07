8358fd60f802e2359706b6ada04d1449a377d544
Akram Idrissi <akram.mo.gb@gmail.com>
2024-12-07 14:31:30 +0100

    Updated security config

--- src/main/java/org/microservice/authservice/config/SecurityConfig.java
+++ src/main/java/org/microservice/authservice/config/SecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 
@@ -16,14 +19,11 @@ public class SecurityConfig {
     @Autowired
     JWTAuthConverter authConverter;
 
-    @Autowired
-    CorsConfig corsConfig;
-
     @Bean
-    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) {
-        http
+    public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity httpSecurity) {
+        httpSecurity
                 .authorizeExchange(authorize -> authorize
-                        .pathMatchers("/eureka/**", "/public/auth/login").permitAll()
+                        .pathMatchers("/eureka/**", "/public/auth/**", "/token/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
@@ -31,11 +31,16 @@ public class SecurityConfig {
                                 .jwtAuthenticationConverter(authConverter)
                         )
                 )
-                .csrf(ServerHttpSecurity.CsrfSpec::disable)
-                .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()));
+                .csrf(ServerHttpSecurity.CsrfSpec::disable);
+                //.cors(cors -> cors.configurationSource(corsConfig.corsConfigurer()));
 
 
-        return http.build();
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(OAuth2ResourceServerProperties properties) {
+        return JwtDecoders.fromIssuerLocation(properties.getJwt().getIssuerUri());
     }
 
 }
